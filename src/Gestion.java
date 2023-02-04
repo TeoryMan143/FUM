85f99fff3128ff943ecdc712ce79cefeac5e29b1
TeoryMan143 <143jonathandavid@gmail.com>
2023-02-04 10:28:57 -0500

    first commit

---
+++ src/Gestion.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Gestion {
+    private JPanel panel1;
+    private JTextField tfName;
+    private JButton btSend;
+    private JTextField tfLname;
+    private JTextField tfDate;
+    private JTextField tfDoc;
+    private JTextField tfDinosaurrr;
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    private JPanel pnMain;
+    private JButton TARIFASYMEMBRESIASButton;
+    private JButton QUIENESSOMOSButton;
+    private JTextArea taInfantiles;
+    private JTextArea taFamilia;
+    private JTextArea textArea1;
+    private JButton button1;
+}
