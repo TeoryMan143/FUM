884a778e200e9cfa8a96713f7a1d428fb6bc7669
TeoryMan <143jonathandavid@gmail.com>
2023-02-06 15:26:06 -0500

    Added onlyNumbers() function

--- src/Execute/Main.java
+++ src/Execute/Main.java
@@ -1,7 +1,7 @@
 package Execute;
 
 import Intefaces.EmpManagement;
-
+import java.awt.event.KeyEvent;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -15,7 +15,6 @@ public class Main {
             return false;
         return pat.matcher(email).matches();
     }
-
     public static void openEmpManagement() {
         EmpManagement empManagement = new EmpManagement();
         Employee employee = empManagement.employee;
@@ -24,6 +23,12 @@ public class Main {
             System.out.println("Se registro el usuario " + employee.name);
         } else {
             System.out.println("Resgistro cancelado");
+        }
+    }
+    public static void onlyNumbers(KeyEvent e) {
+        char c = e.getKeyChar();
+        if (c < '0' || c > '9'){
+            e.consume();
         }
     }
 }
