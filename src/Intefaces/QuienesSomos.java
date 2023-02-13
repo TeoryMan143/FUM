0ee25086f066bcc347408bca00ba26a0a1426f52
TeoryMan <143jonathandavid@gmail.com>
2023-02-13 15:17:37 -0500

    Ya justifiqué el sexo digo texto

--- src/Intefaces/QuienesSomos.java
+++ src/Intefaces/QuienesSomos.java
@@ -28,20 +28,20 @@ public class QuienesSomos extends JFrame{
 
         final String TXT_US = "FUM! es una empresa enfocada en el bienestar y el entretenimiento de sus visitantes\n " +
                 "¡Innovamos cada día para seguir brindando experiencias inolvidables!";
-        final String TXT_MISION = "En una sociedad en la que los sentimientos negativos están presentes en el diario vivir, es \n" +
-                "necesario encontrar una forma de divertirnos y desestresarnos, de relajarnos y pasar un buen \n" +
+        final String TXT_MISION = "En  una sociedad en la que los sentimientos  negativos  están  presentes en el diario  vivir,  es \n" +
+                "necesario encontrar una forma de divertirnos y desestresarnos, de relajarnos y  pasar un buen \n" +
                 "rato.\n\n" +
-                "Por lo tanto, nos enfocamos en crear nuevas atracciones estimulantes y en hacer de nuestros \n" +
+                "Por lo tanto,  nos enfocamos  en crear nuevas atracciones  estimulantes y en hacer de  nuestros \n" +
                 "parques temáticos lugares en los que puedas encontrar espacios de calma y adrenalina. \n\n" +
-                "Fum! es una empresa que existe para brindar alegría y momentos memorables a la vida de\n" +
+                "Fum!  es  una  empresa  que existe  para  brindar  alegría y  momentos  memorables  a la  vida de\n" +
                 "nuestros visitantes, ofreciendo un servicio optimo y gran disposición para que todos puedan tener\n" +
                 "la mejor experiencia.";
-        final String TXT_VISION = "Para el año 2044, ¡FUM! será una empresa reconocida a nivel nacional, ampliando su alcance y\n" +
-                "brindando felicidad a un mayor porcentaje de la población colombiana. Contará con una cadena\n" +
+        final String TXT_VISION = "Para el año 2044, ¡FUM! será una  empresa  reconocida  a nivel nacional,  ampliando su alcance y\n" +
+                "brindando felicidad a un mayor  porcentaje de la  población  colombiana.  Contará con una cadena\n" +
                 "de parques en todo el país. 10 parques temáticos ubicados en las ciudades principales dotados de\n" +
-                "la mejor calidad y servicio para nuestros visitantes, socios y sus familias. ¡Para ese entonces,\n" +
-                "contará con nuevas atracciones enfocadas en la salud mental para hacer de FUM! un lugar de\n" +
-                "disfrute y tranquilidad.";
+                "la mejor calidad y  servicio  para  nuestros visitantes,  socios  y sus familias. \n\n" +
+                "¡Para ese entonces, contará con nuevas  atracciones  enfocadas  en  la  salud mental  para hacer \n" +
+                "de FUM! un lugar de disfrute y tranquilidad.";
 
         taUs.setText(TXT_US);
         taMision.setText(TXT_MISION);
